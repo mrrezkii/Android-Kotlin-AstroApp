0881cbb569cade28cc395820230fd19cc5376c64
nando <muhammad.rezki.ananda@gmail.com>
2021-07-16 10:14:36 +0700

    Add flow layout and hint suggetion

--- app/src/main/java/com/kedirilagi/astro/data/view/fragment/panicbutton/SendMessageFragment.kt
+++ app/src/main/java/com/kedirilagi/astro/data/view/fragment/panicbutton/SendMessageFragment.kt
@@ -1,6 +1,7 @@
 package com.kedirilagi.astro.data.view.fragment.panicbutton
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -29,6 +30,45 @@ class SendMessageFragment : Fragment() {
 
     private fun setupView() {
         viewModel.titleBar.postValue("Layanan Darurat")
+        setupHint()
+    }
+
+    fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
+
+    private fun setupHint() {
+        binding.hint1.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint1.text}").toEditable()
+        }
+        binding.hint2.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint2.text}").toEditable()
+        }
+        binding.hint3.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint3.text}").toEditable()
+        }
+        binding.hint4.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint4.text}").toEditable()
+        }
+        binding.hint5.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint5.text}").toEditable()
+        }
+        binding.hint6.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint6.text}").toEditable()
+        }
+        binding.hint7.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint7.text}").toEditable()
+        }
+        binding.hint8.setOnClickListener {
+            binding.etMessage.text =
+                ("${binding.etMessage.text.toString()} ${binding.hint8.text}").toEditable()
+        }
+
     }
 
 }
