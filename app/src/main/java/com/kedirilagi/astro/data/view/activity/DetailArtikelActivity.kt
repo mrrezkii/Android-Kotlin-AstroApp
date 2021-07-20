d30172f1e41c04e17e425f6b352e259309007727
nando <muhammad.rezki.ananda@gmail.com>
2021-07-20 23:31:19 +0700

    Create webview in detail artikel

--- app/src/main/java/com/kedirilagi/astro/data/view/activity/DetailArtikelActivity.kt
+++ app/src/main/java/com/kedirilagi/astro/data/view/activity/DetailArtikelActivity.kt
@@ -1,10 +1,16 @@
 package com.kedirilagi.astro.data.view.activity
 
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.kedirilagi.astro.databinding.ActivityDetailArtikelBinding
 
 class DetailArtikelActivity : AppCompatActivity() {
+
+    private val judul by lazy { intent.getStringExtra("judul") }
+    private val url by lazy { intent.getStringExtra("url") }
 
     private val binding: ActivityDetailArtikelBinding by lazy {
         ActivityDetailArtikelBinding.inflate(
@@ -12,13 +18,36 @@ class DetailArtikelActivity : AppCompatActivity() {
         )
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setupView()
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun setupView() {
+        binding.tvToolbar.text = judul
+        binding.tvBack.setOnClickListener {
+            finish()
+        }
+        createWebView()
+    }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createWebView() {
+        val loadUrlString = url.toString()
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.apply {
+            loadUrl(loadUrlString)
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (binding.webView.canGoBack()) binding.webView.goBack()
+        else super.onBackPressed()
     }
 }
